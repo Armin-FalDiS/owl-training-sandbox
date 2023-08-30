8c7c4f750b29204a3826c3e8b5279d686dc5ef68
Armin-FalDiS <armin.fallah96@gmail.com>
2023-08-30 18:12:35 +0330

    Implement Counter

--- owl_playground/static/src/counter/counter.js
+++ owl_playground/static/src/counter/counter.js
@@ -1,10 +1,9 @@
 /** @odoo-module **/
 
 import { Component, useState } from '@odoo/owl';
-import { Counter } from './counter/counter';
 
-export class Playground extends Component {
-	static template = 'owl_playground.playground';
+export class Counter extends Component {
+	static template = 'owl_playground.counter';
 
 	setup() {
 		this.state = useState({ value: 0 });
