b81827009a4d8d09636b57897d3bc49ade204eef
Armin-FalDiS <armin.fallah96@gmail.com>
2023-08-31 13:36:43 +0330

    Implement Card

--- owl_playground/static/src/playground.js
+++ owl_playground/static/src/playground.js
@@ -3,11 +3,13 @@
 import { Component } from '@odoo/owl';
 import { Counter } from './counter/counter';
 import { TodoList } from './todo_list/todo_list';
+import { Card } from './card/card';
 
 export class Playground extends Component {
     static template = 'owl_playground.playground';
     static components = {
         Counter,
         TodoList,
+        Card,
     };
 }
