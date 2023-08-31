59863f30e26bbd91f4372ff3ddbeb044d93120b1
Armin-FalDiS <armin.fallah96@gmail.com>
2023-08-31 10:42:37 +0330

    Implement input for Todo list

--- owl_playground/static/src/todo_list/todo_list.js
+++ owl_playground/static/src/todo_list/todo_list.js
@@ -1,15 +1,27 @@
 /** @odoo-module **/
 
-import { Component } from '@odoo/owl';
+import { Component, useState } from '@odoo/owl';
 import { Todo } from '../todo/todo';
 
 export class TodoList extends Component {
     static template = 'owl_playground.todo_list';
     static components = { Todo };
-    static props = {
-        todoList: {
-            type: Array,
-            element: Todo.props.todo,
-        },
-    };
+
+    setup() {
+        this.state = useState({ todoList: [], counter: 1 });
+    }
+
+    addTodo(event) {
+        if (event.target.value && event.keyCode == 13) {
+            this.state.todoList = [
+                ...this.state.todoList,
+                {
+                    id: this.state.counter++,
+                    description: event.target.value,
+                    done: false,
+                },
+            ];
+            event.target.value = null;
+        }
+    }
 }
