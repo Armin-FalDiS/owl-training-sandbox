0747aef5ca158cbdfa93cd27caadf0f06cd6baf0
Armin-FalDiS <armin.fallah96@gmail.com>
2023-09-01 13:51:40 +0330

    Implement Dashboard open orders buttons

--- awesome_tshirt/static/src/dashboard.js
+++ awesome_tshirt/static/src/dashboard.js
@@ -5,6 +5,7 @@ import { registry } from '@web/core/registry';
 import { Layout } from '@web/search/layout';
 import { getDefaultConfig } from '@web/views/view';
 import { useService } from '@web/core/utils/hooks';
+import { Domain } from '@web/core/domain';
 
 class AwesomeDashboard extends Component {
     static template = 'awesome_tshirt.clientaction';
@@ -20,8 +21,31 @@ class AwesomeDashboard extends Component {
         this.action.doAction('base.action_partner_form');
     }
 
-    openNewOrders() {
-        this.action.doAction()
+    openOrders(name, domainStr) {
+        this.action.doAction({
+            type: 'ir.actions.act_window',
+            name,
+            res_model: 'awesome_tshirt.order',
+            domain: new Domain(domainStr).toList(),
+            views: [
+                [false, 'kanban'],
+                [false, 'form'],
+            ],
+        });
+    }
+
+    openRecentOrders() {
+        this.openOrders(
+            'Recent Orders',
+            `[('create_date','>=', (context_today() - datetime.timedelta(days=7)).strftime('%Y-%m-%d'))]`,
+        );
+    }
+
+    openRecentCancelledOrders() {
+        this.openOrders(
+            'Recent Orders',
+            `[('create_date','>=', (context_today() - datetime.timedelta(days=7)).strftime('%Y-%m-%d')), ('state', '=', 'cancelled')]`,
+        );
     }
 }
 
